a4a7a1cdcbb4758376041fa7676586ea06466d38
Pierre Quereuil <80356485+dubitable@users.noreply.github.com>
2021-08-28 17:29:36 -0700

    Added more fig facts

--- components/facts.js
+++ components/facts.js
@@ -1,11 +1,12 @@
 const facts = [
+    "This app fig-ures out whether your photo is a fig or not.",
     "Figs cannot speak English, but they would probably just say obcenities if they could.",
     "New Jersey made figs their national fruit in 1965. Fitting.",
     "Contrary to popular belief, figs are actually vegetables.",
+    "California grows many varieties of figs, but the two most common are the amber-colored, slightly nutty-flavored Golden and the dark purple, sweet Mission.",
+    "Many believe it was figs that were actually the fruit in the Garden of Eden with Adam and Eve, not apples.",
     "All figs possess the same amount of internal seeds. No one has bothered to count them though.",
-    "Figs are directly responsible for the assassination of Archduke Franz Ferdinand.",
     "Romania banned the importation of figs into the country from 1993-1998 due to a suspected illness associated with their consumption.",
-    "This app fig-ures out whether your photo is a fig or not.",
     "According to a 2013 survey held by students of Charles University of Prague, 34% of Czechs present a strong distaste for figs.",
     "A study held in 1971 found that eating too many figs can increase your chances of developing arthritis by the age of 60 by 43%.",
     "Morocco has the highest consumption per capita of figs in the world. Coincidentally, some consider the country among the worst in the world.",
@@ -16,7 +17,10 @@ const facts = [
     "The Roman statesman Cato the Elder once persuaded Rome to go on the Third Punic War against Carthage (146 B.C.) over a fig.",
     "Fig Fossils dating back to 16 Million B.C.E. have recently been discovered by archaelogists in New Mexico.",
     "Watermelon > Figs. By Far.",
-    "Figs are objectively the worst fruit. All those who disagree are wrong."
+    "Figs are objectively the worst fruit. All those who disagree are wrong.",
+    "Chrysippus of Soli, an Ancient Greek Stoic philosopher, allegedly died of laughter after witnessing a donkey eating figs.",
+    "California produces 100% of the nation’s dried figs and 98% of the fresh figs.",
+    
 ]
 
 const toObject = (fact, index) => {
